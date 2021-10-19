d3f8e6831bee65b06ce3a26aca22a3a0755abcc2
Rejoan Islam Afridi <47922447+rejoanafridi@users.noreply.github.com>
2021-10-19 19:08:51 +0600

    initial-commit

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-     return (
-          <div>
-               
-          </div>
-     );
+	return (
+		<div className="login-container">
+			<h1>
+				<span>Login</span> Here
+			</h1>
+			<div className="login-form">
+				<div>
+					<form action="">
+						<p>
+							Email:{" "}
+							<input type="email" name="email" placeholder="Enter your email" />
+						</p>
+						<p>
+							Password:{" "}
+							<input
+								type="password"
+								name="password"
+								placeholder="Enter your Password"
+							/>
+						</p>
+						<input type="submit" value="submit" />
+						<p>
+							don't have an account <Link to="/register">register here</Link>{" "}
+						</p>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
